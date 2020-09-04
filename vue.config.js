b5d01aa6aa13ec587d69774ebd74280ec4e06a7b
rockalen <rockalen0523@gmail.com>
2020-09-04 22:34:55 +0800

    update web title

--- vue.config.js
+++ vue.config.js
@@ -1,3 +1,14 @@
 module.exports = {
-  publicPath: '/js-training-week8/dist/'
+  publicPath: '/js-training-week8/dist/',
+  chainWebpack: (config) => {
+    config.plugin('html').tap((args) => {
+      const [obj] = args
+      obj.title = 'DoSlowly'
+      obj.meta = {
+        keywords: '陶器, 陶杯, 咖啡杯, 木柄, 杯墊, 文創, 手作',
+        description: '手作療癒魔力，打造生活幸福感'
+      }
+      return args
+    })
+  }
 }
